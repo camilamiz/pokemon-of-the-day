016406112a897feb1f5649a6d90218375dd6540e
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-11-24 21:05:30 -0300

    activity mainn layout

--- app/src/main/java/com/camilamizu/pokemon_of_the_day/PokemonInfoActivity.kt
+++ app/src/main/java/com/camilamizu/pokemon_of_the_day/PokemonInfoActivity.kt
@@ -2,10 +2,15 @@ package com.camilamizu.pokemon_of_the_day
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
 
 class PokemonInfoActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_pokemon_info)
+//    val
+//    val url = URL("https://pokeapi.co/api/v2/pokemon/$pokemon/")
+
   }
 }
