30def1171984a46ecdb7ab23491ce694cee480f0
Camila Mizumoto <camila.yuri.m@gmail.com>
2020-11-24 21:53:06 -0300

    Test api get request to pokemon api

--- app/src/main/java/com/camilamizu/pokemon_of_the_day/PokemonInfoActivity.kt
+++ app/src/main/java/com/camilamizu/pokemon_of_the_day/PokemonInfoActivity.kt
@@ -1,7 +1,13 @@
 package com.camilamizu.pokemon_of_the_day
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
 
@@ -9,8 +15,22 @@ class PokemonInfoActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_pokemon_info)
-//    val
-//    val url = URL("https://pokeapi.co/api/v2/pokemon/$pokemon/")
+
+      val pokemonNumber = (0..894).random()
+      val pokemonInfoText : TextView = findViewById(R.id.txvPokemonInfoText)
+      val url = "https://pokeapi.co/api/v2/pokemon/$pokemonNumber/"
+
+      val queue = Volley.newRequestQueue(this)
+
+      val stringRequest = StringRequest(
+        Request.Method.GET, url,
+        Response.Listener<String> {response ->
+          pokemonInfoText.text = "Response is: ${response.substring(0, 500)}"
+        },
+        Response.ErrorListener { pokemonInfoText.text = "That didn't work!" })
+
+      queue.add(stringRequest)
+
 
   }
 }
